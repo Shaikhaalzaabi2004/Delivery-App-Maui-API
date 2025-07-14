cefe14a5d789ebdf3fd1ddbca22e179b30008a04
Shaikha <shaikah321@gmail.com>
2025-07-14 23:02:12 +0400

    update: fixed merge issues

--- Models/DeliveryDatabaseContext.cs
+++ Models/DeliveryDatabaseContext.cs
@@ -29,11 +29,7 @@ public partial class DeliveryDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-<<<<<<< HEAD
         => optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=DELIVERY_DATABASE; TrustServerCertificate=True; Trusted_Connection=True;");
-=======
-        => optionsBuilder.UseSqlServer("Data Source=es-server,1433; Initial Catalog=DELIVERY_DATABASE; TrustServerCertificate=True; User Id=sa; Password=1234567");
->>>>>>> 14606bd9b94dd0ba4473ccb2415bc48032556c71
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
